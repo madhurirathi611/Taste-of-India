8872d23604f2a04f78ec1b27d7b066c7735652c5
Madhuri Mall Rathi <123915361+madhurirathi611@users.noreply.github.com>
2024-05-13 19:25:20 -0400

    Add files via upload

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      TOI ❤, made by{' '}
+      <a
+        href='https://github.com/vishakha-singhal01'
+        target={'_blank'}
+        className='text-blue-500'
+      >
+        👩‍🍳 Madhuri Rathi
+      </a>
+    </div>
+  );
+};
+export default Footer;
+
